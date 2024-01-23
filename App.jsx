1e2abc2e7a879817910786c7f9d5fe094ce0f21e
deepthidas555 <deepthiharidas555@gmail.com>
2024-01-23 16:08:44 +0530

    redux-counter

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="text-center mt-5">
+<h1>counter application</h1>
+<Counter/>
+    </div>
+  );
+}
+
+export default App;
